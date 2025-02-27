0adf52dadaf8beda888e74e467bdc8594ceacedf
ronnie012 <ronnie.247365.1@gmail.com>
2025-02-27 23:09:16 +0600

    Fixed the functionalities of Add Money, Cash Out, & Transaction History.

--- script/addmoney.1.js
+++ script/addmoney.1.js
@@ -1,54 +1,54 @@
 document
-  .getElementById("add-money")
+  .getElementById("add-money-btn")
   .addEventListener("click", function (event) {
     event.preventDefault();
-      const amount = getInputValueByID("amount");
-      const pinNumber = getInputValueByID("Pin");
-      const account = document.getElementById("Account-number").value;
-      const mainBalance = getInnerTextByID("main-balance");
-      console.log(mainBalance);
-      const selectedBank = document.getElementById("allbank").value;
-      console.log(selectedBank)
+    const mainBalance = getInnerTextByID("main-balance");
+    // console.log(mainBalance)
+    const selectedBank = document.getElementById("allbank").value;
+    console.log(selectedBank)
+    const account = document.getElementById("Account-number").value;
+    const amount = getInputValueByID("amount");
+    const pinNumber = getInputValueByID("Pin");
 
-      if (selectedBank==="Select MFS") {
-        alert("Please Select an MFS.")
-        return;
-      }
-      if (amount<0) {
-        alert("Enter amount greater than 0.")
-        return;
-      }
+    if (selectedBank === "Select MFS") {
+      alert("Please Select an MFS.")
+      return;
+    }
 
-      if (account.length===11) {
-        if (pinNumber===1234) {
-          const sum = mainBalance + amount;
-          // document.getElementById("main-balance").innerText = sum;
-          setInnerTextByIdAndValue("main-balance", sum)
+    if (amount < 0 || amount > mainBalance || isNaN(amount)) {
+      alert("Enter only numbers greater than 0 and less than balance.");
+      return;
+    }
 
-          const container = document.getElementById("transaction-container");
+    if (account.length === 11) {
+      if (pinNumber === 1234) {
+        const sum = mainBalance + amount;
+        // document.getElementById("main-balance").innerText = sum;
+        setInnerTextByIdAndValue("main-balance", sum);
 
-          const div = document.createElement("div");
-          div.classList.add("bg-red-400")
-          div.innerHTML = `
+        const container = document.getElementById("transaction-container");
+
+        const div = document.createElement("div");
+        div.classList.add("bg-red-400")
+        div.innerHTML = `
           <h1 class="text-green-400">Credited $${amount} from ${selectedBank} account number: ${account}.</h1>
-          <h3>
           `
-          container.appendChild(div)
-          // div.preventDefault();
-          /*
-          const p = document.createElement("p");
-          p.innerText=`Your Account was Credited by \$${amount} from ${account};`
-          console.log(p)
-          container.appendChild(p);
-          */
-        }
-        else {
-          alert("Wrong Pin, Enter Correct PIN.")
-        }
+        container.appendChild(div)
+        // div.preventDefault();
+        /*
+        const p = document.createElement("p");
+        p.innerText=`Your Account was Credited by \$${amount} from ${account};`
+        console.log(p)
+        container.appendChild(p);
+        */
       }
       else {
-        alert("Enter Valid 11-digit Acount Number.")
+        alert("Wrong Pin, Enter Correct PIN.")
       }
+    }
+    else {
+      alert("Enter Valid 11-digit Account Number.")
+    }
 
 
-});
+  });
--- script/cashout.1.js
+++ script/cashout.1.js
@@ -1,37 +1,48 @@
 document
-  .getElementById("cashout-btn")
+  .getElementById("cash-out-btn")
   .addEventListener("click", function (event) {
     event.preventDefault();
+    const mainBalance = getInnerTextByID("main-balance");
+    // console.log(mainBalance)
     const accountNumber = document.getElementById("Account-number").value;
     const amount = getInputValueByID("cashout-amount");
     const pinNumber = getInputValueByID("cashout-pin");
-    const mainBalance = getInnerTextByID("main-balance");
-    console.log(amount)
 
-    if (amount>mainBalance) {
-      alert("Enter amount less than or equal to balance.")
+    if (amount>mainBalance || amount<0 || isNaN(amount)) {
+      alert("Enter only numbers greater than 0 and less than balance.")
       return;
     }
 
-    if (accountNumber.length===11) {
-      if (pinNumber===1234) {
+    if (accountNumber.length === 11) {
+      if (pinNumber === 1234) {
         const sum = mainBalance - amount;
         setInnerTextByIdAndValue("main-balance", sum);
 
+        const container = document.getElementById("transaction-container");
+
+        const div = document.createElement("div");
+          div.classList.add("bg-red-400")
+          div.innerHTML = `
+          <h1 class="text-green-400">Debited $${amount} from account number: ${accountNumber}.
+          </h1>
+          `
+          container.appendChild(div)
+
+        /*
         const p = document.createElement("p");
         p.innerText=`Your Account was Debited by \$${amount} to ${accountNumber};`
         console.log(p)
 
-        const container = document.getElementById("transaction-container");
         container.appendChild(p);
+        */
       }
       else {
-        ("Enter valid PIN.")
+        alert("Wrong Pin, Enter Correct PIN.")
       }
     }
     else {
-      alert("Enter Valid Amount greater than 0 and less than or equal to 4500")
+      alert("Enter Valid 11-digit Account Number.")
     }
 
 
-})
+});
